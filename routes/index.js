fc33150120a84fe3e5b52354ee6ebcd7bc697889
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2015-02-05 22:59:00 +0800

    now we can access front-end view from server

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var React = require('react');
+
+var path = require('path');
+
+var frontEndPath = path.join(__dirname, '../front_end', 'scripts').toString();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var Greeting = require(frontEndPath + '/greeting');
+  console.log(Greeting);
   res.render('index', { title: 'Express' });
 });
 
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+// to require jsx module
+require('node-jsx').install();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
