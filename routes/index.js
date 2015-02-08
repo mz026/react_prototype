f96365c87baa08ea6024aacebb11674a9d1e4d58
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2015-02-08 14:42:52 +0800

    render react component on server side!

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,18 @@ var path = require('path');
 
 var frontEndPath = path.join(__dirname, '../front_end', 'scripts').toString();
 
+var fs = require('fs');
+var ejs = require('ejs');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var Greeting = require(frontEndPath + '/greeting');
-  console.log(Greeting);
-  res.render('index', { title: 'Express' });
+
+  template = fs.readFileSync(__dirname + '/../front_end/index.html', 'utf8');
+  console.log(template);
+
+  var content = React.renderToString(Greeting({ name: 'Jude' }));
+  res.send(ejs.render(template, { content: content }));
 });
 
 module.exports = router;
