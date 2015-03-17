8f52d0431080a370d9f9d2bbee9b8ab4465318f2
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2015-03-17 16:09:04 +0800

    add external api using node

---
+++ external_api/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+
+var server = express();
+server.get('/experts', function(req, res) {
+  var experts = [];
+  for (var i = 0; i < 20; i += 1) {
+    var index = i + 1;
+    experts.push({
+      id: index,
+      name: 'mentor-' + index,
+      age: index + 20
+    });
+  };
+  res.json(experts);
+});
+
+
+server.get('/lessons', function(req, res) {
+  var lessons = [];
+  for (var i = 0; i < 20; i += 1) {
+    var index = i + 1;
+    lessons.push({
+      id: index,
+      name: 'lesson-' + index,
+      mentor_id: index
+    });
+  };
+  res.json(lessons);
+});
+
+console.log('server listen to port: 3001');
+server.listen(3001);
+
